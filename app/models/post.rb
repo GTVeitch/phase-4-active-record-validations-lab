e957eed50d071be88d39328af04a7c18c65e908c
Gavin Veitch <gavintveitch@gmail.com>
2022-11-29 09:10:02 -0700

    commit tree a553edbd85266719bc465b49204c83bdcc0202ce

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validate :is_clickbait
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    def is_clickbait
+        if title.present?
+            if !((title.include? "Won't Believe") || (title.include? "Secret") || (title.include? "Top ") || (title.include? "Guess"))
+                errors.add(:title, "Not Clickbait enough")
+            end
+        end
+    end
+
 end
